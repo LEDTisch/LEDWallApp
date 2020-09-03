5ae7e763d8dad0f3f9c4437c9d4a2b13695ed6e8
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-09-03 13:43:52 +0200

    appsteuerung

--- app/src/main/java/de/ft/LEDWallApp/ui/snake/Snake.java
+++ app/src/main/java/de/ft/LEDWallApp/ui/snake/Snake.java
@@ -8,10 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import de.ft.LEDWallApp.Connection;
 import de.ft.LEDWallApp.R;
 
 public class Snake extends Fragment {
@@ -25,8 +28,42 @@ public class Snake extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.snake_fragment, container, false);
-    }
+
+
+        View view =  inflater.inflate(R.layout.snake_fragment, container, false);
+        Button hoch = view.findViewById(R.id.hoch);
+        Button runter = view.findViewById(R.id.runter);
+        Button links = view.findViewById(R.id.links);
+        Button rechts = view.findViewById(R.id.rechts);
+        hoch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connection.send("h");
+            }
+        });
+        runter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connection.send("d");
+            }
+        });
+        rechts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connection.send("r");
+            }
+        });
+        links.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connection.send("l");
+            }
+        });
+
+
+
+
+        return view;    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
