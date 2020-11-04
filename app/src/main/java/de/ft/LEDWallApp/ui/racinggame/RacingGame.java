9cf1dfc6925c17e0d71d532af7b113252073a1bb
Coder246 <tim-arnold@outlook.de>
2020-11-04 10:25:09 +0100

    Added Icons

---
+++ app/src/main/java/de/ft/LEDWallApp/ui/racinggame/RacingGame.java
@@ -0,0 +1,69 @@
+package de.ft.LEDWallApp.ui.racinggame;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import de.ft.LEDWallApp.Connection;
+import de.ft.LEDWallApp.R;
+
+public class RacingGame extends Fragment {
+
+    private RacingGameViewModel mViewModel;
+
+    public static RacingGame newInstance() {
+        return new RacingGame();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.racing_game_fragment, container, false);
+
+        Button left = view.findViewById(R.id.links);
+        Button right = view.findViewById(R.id.rechts);
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Connection.send();
+
+            }
+        });
+
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                Connection.send();
+
+
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(RacingGameViewModel.class);
+        // TODO: Use the ViewModel
+
+    }
+
+}
