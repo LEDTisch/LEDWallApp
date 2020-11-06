0eac227917f9958bb8e492c034aaf87e41e418fa
Coder246 <tim-arnold@outlook.de>
2020-11-06 13:09:30 +0100

    Man kann die App wieder benutzen ohne Augenkrebs zu bekommen

--- app/src/main/java/de/ft/LEDWallApp/MainActivity.java
+++ app/src/main/java/de/ft/LEDWallApp/MainActivity.java
@@ -2,6 +2,7 @@ package de.ft.LEDWallApp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -47,10 +48,14 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
 
         toolbar = findViewById(R.id.toolbar);
